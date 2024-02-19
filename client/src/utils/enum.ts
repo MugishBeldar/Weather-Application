421e15f0c9295d545076cfbf267b46afad352344
MugishBeldar <mugishbeldar333@gmail.com>
2024-02-19 18:14:11 +0530

    server hosted

--- client/src/utils/enum.ts
+++ client/src/utils/enum.ts
@@ -1,4 +1,5 @@
-const API_ENDPOINT_V1 = "http://localhost:5000/api/v1";
+// const API_ENDPOINT_V1 = "http://localhost:5000/api/v1";
+const API_ENDPOINT_V1 = "https://breeze-i9od.onrender.com/api/v1";
 const API_ENDPOINT = API_ENDPOINT_V1;
 const STATISTICS_API = {
   GET_WEATHERDETAILS: `${API_ENDPOINT}/retriveweatherdata`,
